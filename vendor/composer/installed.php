da7061834f29aa9a49bd1ae90333508910e3be70
swagat-dash <swagat308@gmail.com>
2021-07-22 17:02:27 +0530

    dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '13e7419aa817e28f379d66d7e6fb7f0603b31404',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '13e7419aa817e28f379d66d7e6fb7f0603b31404',
+    ),
+  ),
+);
