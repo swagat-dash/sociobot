7e59284950ba98427ba800a2d5d05a27dd84ea75
swagat-dash <swagat308@gmail.com>
2021-07-22 15:30:51 +0530

    php gd, mbstring, exif enabled

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '129bfa46ba45a970ca791ee921f8efa0b4705b4c',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '129bfa46ba45a970ca791ee921f8efa0b4705b4c',
+    ),
+  ),
+);
