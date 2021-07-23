80132bfba37efe490a725936ac9b8f4dc40c3940
swagat-dash <swagat308@gmail.com>
2021-07-23 17:42:31 +0530

    php ext

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '110ea1e68d677b853ebd2c3c7187a5e6733e49db',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '110ea1e68d677b853ebd2c3c7187a5e6733e49db',
+    ),
+  ),
+);
